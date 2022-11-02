0ca56f4977ce7f19b593427c06ab1301b198c5a7
Maria De Sousa Ardiet <93591663+Maria-dsa@users.noreply.github.com>
2022-11-02 15:01:22 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,7 +1,17 @@
+<?php require 'inc/head.php';
+require 'inc/data/products.php';
+
+?>
+<?php session_start(); ?>
 <?php require 'inc/head.php'; ?>
 <section class="cookies container-fluid">
     <div class="row">
-        TODO : Display shopping cart items from $_SESSION here.
+        <?php
+        $cart = array_count_values($_SESSION['cart']);
+        foreach ($cart as $key => $value) {
+            echo $value . ' of ' . $catalog[$key]['name'] . '<br>';
+        }
+        ?>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
