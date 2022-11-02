98d2b7eef4999d2ef31eaf480ef3b569cd8dc217
Maria De Sousa Ardiet <93591663+Maria-dsa@users.noreply.github.com>
2022-11-02 15:00:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php require 'inc/data/products.php'; ?>
 <?php require 'inc/head.php'; ?>
+
+<?php
+session_start();
+if (isset($_GET['add_to_cart'])) {
+    $_SESSION['cart'][] = $_GET['add_to_cart'];
+}
+?>
+
 <section class="cookies container-fluid">
     <div class="row">
         <?php foreach ($catalog as $id => $cookie) { ?>
